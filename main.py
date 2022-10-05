29e82f3d97282c718fcc2ab06e7f84f871855e6c
Zanetar <108569850+Zanetar@users.noreply.github.com>
2022-10-05 18:23:14 +0200

    Update main.py

--- main.py
+++ main.py
@@ -83,5 +83,30 @@ def division():
         return
     except ZeroDivisionError: print('Nie dziel przez zero!')
     except ValueError: print('Błędnie wprowadzony znak!')
-#dopisz manu
 
+def menu():
+    try:
+        while True:
+            print('Witaj w kalkulatorze')
+            print('Co chcesz zrobić?')
+            print('1----dodawanie')
+            print('2----odejmowanie')
+            print('3----mnożenie')
+            print('4----dzielenie')
+            print('0----wyjście')
+            choice=int(input())
+            if  choice==1:
+                plus()
+            elif choice==2:
+                minus()
+            elif choice==3:
+                multi()
+            elif choice==4:
+                division()
+            elif choice==0:
+                break
+            else:
+                print('Wybrano zły znak!')
+    except: print ('Wybrano zły znak!')
+
+menu() #wywołanie kalkulatora
